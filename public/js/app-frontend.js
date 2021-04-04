c6fc9626adbbb5ddddd29aa1f40aed290702bfee
developers-bmotion <54631586+developers-bmotion@users.noreply.github.com>
2021-04-04 01:14:24 -0500

    Ajuste de tarjetas

--- public/js/app-frontend.js
+++ public/js/app-frontend.js
@@ -8416,6 +8416,12 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
 
 
 /* harmony default export */ __webpack_exports__["default"] = ({
@@ -8427,7 +8433,7 @@ __webpack_require__.r(__webpack_exports__);
   data: function data() {
     return {
       options: {
-        gap: '1rem',
+        // gap: '1rem',
         focus: 'center',
         perPage: 2,
         pagination: false,
@@ -8655,6 +8661,25 @@ exports = module.exports = __webpack_require__(/*! ../../../../../node_modules/c
 
 // module
 exports.push([module.i, "\n.card-title.text-title-card-mobile {\n    /*display: block;*/\n    /*width: 100px;*/\n    overflow: hidden;\n    white-space: nowrap;\n    text-overflow: ellipsis;\n}\n", ""]);
+
+// exports
+
+
+/***/ }),
+
+/***/ "./node_modules/css-loader/index.js?!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/components/change-theme-frontend.vue?vue&type=style&index=0&lang=css&":
+/*!**************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader??ref--7-1!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src??ref--7-2!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/components/change-theme-frontend.vue?vue&type=style&index=0&lang=css& ***!
+  \**************************************************************************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(/*! ../../../../node_modules/css-loader/lib/css-base.js */ "./node_modules/css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "\n#splide02-slide01{\n    width: 7% !important;\n}\n#splide02-slide02{\n     width: 7% !important;\n}\n#splide02-slide03{\n     width: 7% !important;\n}\n", ""]);
 
 // exports
 
@@ -26482,6 +26507,36 @@ if(false) {}
 
 /***/ }),
 
+/***/ "./node_modules/style-loader/index.js!./node_modules/css-loader/index.js?!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/components/change-theme-frontend.vue?vue&type=style&index=0&lang=css&":
+/*!******************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/style-loader!./node_modules/css-loader??ref--7-1!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src??ref--7-2!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/components/change-theme-frontend.vue?vue&type=style&index=0&lang=css& ***!
+  \******************************************************************************************************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+var content = __webpack_require__(/*! !../../../../node_modules/css-loader??ref--7-1!../../../../node_modules/vue-loader/lib/loaders/stylePostLoader.js!../../../../node_modules/postcss-loader/src??ref--7-2!../../../../node_modules/vue-loader/lib??vue-loader-options!./change-theme-frontend.vue?vue&type=style&index=0&lang=css& */ "./node_modules/css-loader/index.js?!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/components/change-theme-frontend.vue?vue&type=style&index=0&lang=css&");
+
+if(typeof content === 'string') content = [[module.i, content, '']];
+
+var transform;
+var insertInto;
+
+
+
+var options = {"hmr":true}
+
+options.transform = transform
+options.insertInto = undefined;
+
+var update = __webpack_require__(/*! ../../../../node_modules/style-loader/lib/addStyles.js */ "./node_modules/style-loader/lib/addStyles.js")(content, options);
+
+if(content.locals) module.exports = content.locals;
+
+if(false) {}
+
+/***/ }),
+
 /***/ "./node_modules/style-loader/index.js!./node_modules/css-loader/index.js?!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/frontend/pages/RegisterClient.vue?vue&type=style&index=0&lang=css&":
 /*!***************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/style-loader!./node_modules/css-loader??ref--7-1!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src??ref--7-2!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/frontend/pages/RegisterClient.vue?vue&type=style&index=0&lang=css& ***!
@@ -31820,10 +31875,10 @@ render._withStripped = true
 
 /***/ }),
 
-/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/components/change-theme-frontend.vue?vue&type=template&id=0fc5fe65&scoped=true&":
-/*!***********************************************************************************************************************************************************************************************************************************************!*\
-  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/components/change-theme-frontend.vue?vue&type=template&id=0fc5fe65&scoped=true& ***!
-  \***********************************************************************************************************************************************************************************************************************************************/
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/components/change-theme-frontend.vue?vue&type=template&id=0fc5fe65&":
+/*!***********************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/components/change-theme-frontend.vue?vue&type=template&id=0fc5fe65& ***!
+  \***********************************************************************************************************************************************************************************************************************************/
 /*! exports provided: render, staticRenderFns */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
@@ -53924,9 +53979,11 @@ __webpack_require__.r(__webpack_exports__);
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _change_theme_frontend_vue_vue_type_template_id_0fc5fe65_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./change-theme-frontend.vue?vue&type=template&id=0fc5fe65&scoped=true& */ "./resources/js/components/components/change-theme-frontend.vue?vue&type=template&id=0fc5fe65&scoped=true&");
+/* harmony import */ var _change_theme_frontend_vue_vue_type_template_id_0fc5fe65___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./change-theme-frontend.vue?vue&type=template&id=0fc5fe65& */ "./resources/js/components/components/change-theme-frontend.vue?vue&type=template&id=0fc5fe65&");
 /* harmony import */ var _change_theme_frontend_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./change-theme-frontend.vue?vue&type=script&lang=js& */ "./resources/js/components/components/change-theme-frontend.vue?vue&type=script&lang=js&");
-/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* empty/unused harmony star reexport *//* harmony import */ var _change_theme_frontend_vue_vue_type_style_index_0_lang_css___WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./change-theme-frontend.vue?vue&type=style&index=0&lang=css& */ "./resources/js/components/components/change-theme-frontend.vue?vue&type=style&index=0&lang=css&");
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
 
 
 
@@ -53934,13 +53991,13 @@ __webpack_require__.r(__webpack_exports__);
 
 /* normalize component */
 
-var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_3__["default"])(
   _change_theme_frontend_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
-  _change_theme_frontend_vue_vue_type_template_id_0fc5fe65_scoped_true___WEBPACK_IMPORTED_MODULE_0__["render"],
-  _change_theme_frontend_vue_vue_type_template_id_0fc5fe65_scoped_true___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
+  _change_theme_frontend_vue_vue_type_template_id_0fc5fe65___WEBPACK_IMPORTED_MODULE_0__["render"],
+  _change_theme_frontend_vue_vue_type_template_id_0fc5fe65___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
   false,
   null,
-  "0fc5fe65",
+  null,
   null
   
 )
@@ -53966,19 +54023,35 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./resources/js/components/components/change-theme-frontend.vue?vue&type=template&id=0fc5fe65&scoped=true&":
-/*!*****************************************************************************************************************!*\
-  !*** ./resources/js/components/components/change-theme-frontend.vue?vue&type=template&id=0fc5fe65&scoped=true& ***!
-  \*****************************************************************************************************************/
+/***/ "./resources/js/components/components/change-theme-frontend.vue?vue&type=style&index=0&lang=css&":
+/*!*******************************************************************************************************!*\
+  !*** ./resources/js/components/components/change-theme-frontend.vue?vue&type=style&index=0&lang=css& ***!
+  \*******************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_style_loader_index_js_node_modules_css_loader_index_js_ref_7_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_2_node_modules_vue_loader_lib_index_js_vue_loader_options_change_theme_frontend_vue_vue_type_style_index_0_lang_css___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/style-loader!../../../../node_modules/css-loader??ref--7-1!../../../../node_modules/vue-loader/lib/loaders/stylePostLoader.js!../../../../node_modules/postcss-loader/src??ref--7-2!../../../../node_modules/vue-loader/lib??vue-loader-options!./change-theme-frontend.vue?vue&type=style&index=0&lang=css& */ "./node_modules/style-loader/index.js!./node_modules/css-loader/index.js?!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/components/change-theme-frontend.vue?vue&type=style&index=0&lang=css&");
+/* harmony import */ var _node_modules_style_loader_index_js_node_modules_css_loader_index_js_ref_7_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_2_node_modules_vue_loader_lib_index_js_vue_loader_options_change_theme_frontend_vue_vue_type_style_index_0_lang_css___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_index_js_node_modules_css_loader_index_js_ref_7_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_2_node_modules_vue_loader_lib_index_js_vue_loader_options_change_theme_frontend_vue_vue_type_style_index_0_lang_css___WEBPACK_IMPORTED_MODULE_0__);
+/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _node_modules_style_loader_index_js_node_modules_css_loader_index_js_ref_7_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_2_node_modules_vue_loader_lib_index_js_vue_loader_options_change_theme_frontend_vue_vue_type_style_index_0_lang_css___WEBPACK_IMPORTED_MODULE_0__) if(["default"].indexOf(__WEBPACK_IMPORT_KEY__) < 0) (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _node_modules_style_loader_index_js_node_modules_css_loader_index_js_ref_7_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_2_node_modules_vue_loader_lib_index_js_vue_loader_options_change_theme_frontend_vue_vue_type_style_index_0_lang_css___WEBPACK_IMPORTED_MODULE_0__[key]; }) }(__WEBPACK_IMPORT_KEY__));
+
+
+/***/ }),
+
+/***/ "./resources/js/components/components/change-theme-frontend.vue?vue&type=template&id=0fc5fe65&":
+/*!*****************************************************************************************************!*\
+  !*** ./resources/js/components/components/change-theme-frontend.vue?vue&type=template&id=0fc5fe65& ***!
+  \*****************************************************************************************************/
 /*! exports provided: render, staticRenderFns */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_change_theme_frontend_vue_vue_type_template_id_0fc5fe65_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../../node_modules/vue-loader/lib??vue-loader-options!./change-theme-frontend.vue?vue&type=template&id=0fc5fe65&scoped=true& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/components/change-theme-frontend.vue?vue&type=template&id=0fc5fe65&scoped=true&");
-/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_change_theme_frontend_vue_vue_type_template_id_0fc5fe65_scoped_true___WEBPACK_IMPORTED_MODULE_0__["render"]; });
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_change_theme_frontend_vue_vue_type_template_id_0fc5fe65___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../../node_modules/vue-loader/lib??vue-loader-options!./change-theme-frontend.vue?vue&type=template&id=0fc5fe65& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/components/change-theme-frontend.vue?vue&type=template&id=0fc5fe65&");
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_change_theme_frontend_vue_vue_type_template_id_0fc5fe65___WEBPACK_IMPORTED_MODULE_0__["render"]; });
 
-/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_change_theme_frontend_vue_vue_type_template_id_0fc5fe65_scoped_true___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_change_theme_frontend_vue_vue_type_template_id_0fc5fe65___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
 
 
 
--- resources/js/components/components/change-theme-frontend.vue
+++ resources/js/components/components/change-theme-frontend.vue
@@ -5,25 +5,31 @@
                 <splide :options="options">
                     <splide-slide>
                         <div class="card card-info-animation">
-                            <img class="card-img-top pt-1 pr-1 pl-1" src="/images/theme/img-light-mode.png" alt="Card image cap">
+                            <img class="card-img-top pt-1 pr-1 pl-1" src="/images/theme/img-light-mode.png"
+                                 alt="Card image cap">
                             <div class="card-body" style="padding-top: 1rem !important;">
-                                <h6 class="card-title text-center text-title-card-mobile" style="font-size: 1.2rem; margin-bottom: 0.30rem !important;">Light Mode</h6>
+                                <h6 class="card-title text-center text-title-card-mobile"
+                                    style="font-size: 1.2rem; margin-bottom: 0.30rem !important;">Light Mode</h6>
                             </div>
                         </div>
                     </splide-slide>
                     <splide-slide>
                         <div class="card card-info-animation">
-                            <img class="card-img-top pt-1 pr-1 pl-1" src="/images/theme/img-dark-mode.png" alt="Card image cap">
+                            <img class="card-img-top pt-1 pr-1 pl-1" src="/images/theme/img-dark-mode.png"
+                                 alt="Card image cap">
                             <div class="card-body" style="padding-top: 1rem !important;">
-                                <h6 class="card-title text-center text-title-card-mobile" style="font-size: 1.2rem; margin-bottom: 0.30rem !important;">Dark Mode</h6>
+                                <h6 class="card-title text-center text-title-card-mobile"
+                                    style="font-size: 1.2rem; margin-bottom: 0.30rem !important;">Dark Mode</h6>
                             </div>
                         </div>
                     </splide-slide>
                     <splide-slide>
                         <div class="card card-info-animation">
-                            <img class="card-img-top pt-1 pr-1 pl-1" src="/images/theme/img-system-theme.png" alt="Card image cap">
+                            <img class="card-img-top pt-1 pr-1 pl-1" src="/images/theme/img-system-theme.png"
+                                 alt="Card image cap">
                             <div class="card-body" style="padding-top: 1rem !important;">
-                                <h6 class="card-title text-center text-title-card-mobile" style="font-size: 1.2rem; margin-bottom: 0.30rem !important;">Según el sistema</h6>
+                                <h6 class="card-title text-center text-title-card-mobile"
+                                    style="font-size: 1.2rem; margin-bottom: 0.30rem !important;">Según el sistema</h6>
                             </div>
                         </div>
                     </splide-slide>
@@ -37,8 +43,9 @@
 <script>
 import {Splide, SplideSlide} from '@splidejs/vue-splide';
 import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+
 export default {
-name: "modal-theme-frontend",
+    name: "modal-theme-frontend",
     components: {
         Splide,
         SplideSlide,
@@ -46,8 +53,8 @@ name: "modal-theme-frontend",
     data() {
         return {
             options: {
-                gap: '1rem',
-                focus : 'center',
+                // gap: '1rem',
+                focus: 'center',
                 perPage: 2,
                 pagination: false,
                 trimSpace: false,
@@ -60,6 +67,15 @@ name: "modal-theme-frontend",
 }
 </script>
 
-<style scoped>
+<style>
 
+   #splide02-slide01{
+       width: 7% !important;
+   }
+    #splide02-slide02{
+        width: 7% !important;
+    }
+    #splide02-slide03{
+        width: 7% !important;
+    }
 </style>
